02ac195e2a5168a29ff449bd7e200484916db8ba
Love <nguyenthitinh.ou@gmail.com>
2021-05-05 22:03:09 +0700

    love: commit 3

--- services/OrganizationService.java
+++
@@ -1,8 +0,0 @@
-package com.mycompany.tokointest.services;
-
-import com.mycompany.tokointest.models.Organization;
-import com.mycompany.tokointest.models.OrganizationField;
-
-public interface OrganizationService extends SearchService<Organization, OrganizationField> {
-
-}
